786e3b6ebdd7da3819901753e9b8170f79b97725
Ricardo Gonzalez <Ric.gonzalezs@duocuc.cl>
2026-09-20 14:36:42 -0300

    Login y vistas principales de usuario y administrador

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function iniciarSesion() {
+
+    let usuario = document.getElementById("usuario").value;
+    let contrasena = document.getElementById("contrasena").value;
+
+    if (usuario == "" || contrasena == "") {
+
+        alert("Debe ingresar usuario y contraseña");
+
+    } else if (usuario == "duoc" && contrasena == "1234") {
+
+        window.location.href = "usuario/home.html";
+
+    } else if (usuario == "admin" && contrasena == "1234") {
+
+        window.location.href = "admin/home.html";
+
+    } else {
+
+        alert("Usuario o contraseña incorrectos");
+
+    }
+
+}
