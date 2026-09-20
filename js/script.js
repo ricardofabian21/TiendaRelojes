8b3a5f01e37130a905e392634e67ee05dde5ede5
Ricardo Gonzalez <Ric.gonzalezs@duocuc.cl>
2026-09-20 17:57:19 -0300

    mejorar vistas de administrador y usuario

--- js/script.js
+++ js/script.js
@@ -1,23 +1,45 @@
 function iniciarSesion() {
 
-    let usuario = document.getElementById("usuario").value;
+    let correo = document.getElementById("correo").value;
     let contrasena = document.getElementById("contrasena").value;
 
-    if (usuario == "" || contrasena == "") {
+    if (correo == "") {
 
-        alert("Debe ingresar usuario y contraseña");
+        alert("Debe ingresar su correo");
 
-    } else if (usuario == "duoc" && contrasena == "1234") {
+    } else if (correo.length > 100) {
+
+        alert("El correo no puede tener más de 100 caracteres");
+
+    } else if (
+        !correo.endsWith("@duoc.cl") &&
+        !correo.endsWith("@profesor.duoc.cl") &&
+        !correo.endsWith("@gmail.com") &&
+        !correo.endsWith("@icloud.com") &&
+        !correo.endsWith("@hotmail.com")
+    ) {
+
+        alert("Debe ingresar un correo válido");
+
+    } else if (contrasena == "") {
+
+        alert("Debe ingresar su contraseña");
+
+    } else if (contrasena.length < 4 || contrasena.length > 10) {
+
+        alert("La contraseña debe tener entre 4 y 10 caracteres");
+
+    } else if (correo == "duoc@duoc.cl" && contrasena == "1234") {
 
         window.location.href = "../usuario/home.html";
 
-    } else if (usuario == "admin" && contrasena == "1234") {
+    } else if (correo == "admin@duoc.cl" && contrasena == "1234") {
 
         window.location.href = "../admin/home.html";
 
     } else {
 
-        alert("Usuario o contraseña incorrectos");
+        alert("Correo o contraseña incorrectos");
 
     }
 
